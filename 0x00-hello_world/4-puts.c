5047d945985bab3fabbe776cd0279bb349f5d708
Racheal <adenike.bam07@gmail.com>
2022-10-13 07:50:45 -0700

    Committed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *main - Printing in C
+ *
+ *Return: Always 0 (It is successful)
+ */
+int main(void)
+{
+     puts("\"Programming is like building a multilingual puzzle");
+     return(0)
+}
+
